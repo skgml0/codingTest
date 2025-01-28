3e35b9ac60dd937a180af61638ddeb45cc04e7ae
hee <52391780+skgml0@users.noreply.github.com>
2025-01-28 18:48:30 +0900

    [level 2] Title: 숫자 변환하기, Time: 127.88 ms, Memory: 59.1 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,10 +1,27 @@
 function solution(x, y, n) {
-    const dp = new Array(y+1).fill(Infinity);
-    dp[x] = 0;
-    for(let i=x; i<y; i++){
-        dp[i+n] = Math.min(dp[i+n],dp[i]+1);
-        dp[i*2] = Math.min(dp[i*2],dp[i]+1);
-        dp[i*3] = Math.min(dp[i*3],dp[i]+1);
+    if(x===y) return 0;
+    const queue = new Map([[x,0]])
+    // const queue = new Map([[x, 0]]);  // key: 숫자, value: 연산 횟수
+    let index = 0;
+    const arr = [x]; // queue 역할 
+    // while (index < arr.length) {
+        
+//         const cur = arr[index++];
+//         console.log(cur,'cur----')
+//         const cnt = queue.get(cur);
+            for (const [cur, cnt] of queue) {
+
+
+        for (const next of [cur + n, cur * 2, cur * 3]) {
+            // console.log(next,'next',queue,'queue========')
+            // console.log(arr,'arr====2')
+            
+            if (next === y) return cnt + 1;
+            if (next > y || queue.has(next)) continue;
+            
+            queue.set(next, cnt + 1);
+        }
     }
-    return dp[y]!==Infinity? dp[y] : -1;
+    
+    return -1;
 }
