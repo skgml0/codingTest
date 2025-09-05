19fae8ee9fc97a73cbf9e4516243b3f925a51667
hee <52391780+skgml0@users.noreply.github.com>
2025-09-05 16:07:28 +0900

    [level 2] Title: 의상, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,10 @@
+function solution(clothes) {
+    const types = {}
+    let sum = 1
+    for (const [style,type] of clothes){
+        if(type in types) types[type] +=1
+        else types[type]=1;
+    }
+    for (const type in types) sum *= types[type] +1
+    return sum -1   
+}
